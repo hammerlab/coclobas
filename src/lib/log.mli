e07d518fe2566b2306bc3b688e94a6bfce7217c9
Sebastien Mondet <seb@mondet.org>
2016-08-17 18:17:13 -0400

    Add `Log.debug_sections`
    This allows to also print some messages to `stdout` when needed.

--- src/lib/log.mli
+++ src/lib/log.mli
@@ -10,3 +10,12 @@ val log :
   Yojson.Safe.json ->
   (unit,
    [> `Storage of [> Storage.Error.common ] ]) Deferred_result.t
+
+
+val debug_sections : string list list ref
+(** Catch sections and display them on ["stdout"], cf. the [?section] argument of {!log}.
+
+    At the start of the program, the [Log] module tries to parse
+    the ["COCLOBAS_DEBUG_SECTIONS"] environment variable to fill this
+    reference (coma-separated list of paths, e.g. ["/server/loop,cluster//commands"]).
+ *)
