d108972feb40cd05ac5336492e6dfcde5271f31e
Sebastien Mondet <seb@mondet.org>
2016-09-21 15:08:10 +0000

    Allow some configuration of the server
    This allows to mitigate issues like #42 in environments where Kubernetes
    can be a bit unreliable.

--- src/lib/server.mli
+++ src/lib/server.mli
@@ -1,9 +1,40 @@
 open Internal_pervasives
 
+module Configuration : sig
+
+  module Default : sig
+    val min_sleep : float
+    val max_sleep : float
+    val max_update_errors : int
+  end
+
+  type t = {
+    min_sleep: float [@default Default.min_sleep];
+    max_sleep: float [@default Default.max_sleep];
+    max_update_errors: int [@default Default.max_update_errors];
+  } [@@deriving make, yojson, show]
+
+  val save :
+    storage:Storage.t ->
+    t ->
+    (unit, [> `Storage of [> Storage.Error.common ] ])
+      Deferred_result.t
+  val get :
+    Storage.t ->
+    (t,
+     [> `Storage of
+          [> Storage.Error.common
+          | `Missing_data of string
+          | `Of_json of string ] ])
+      Deferred_result.t
+end
+
+
 type t
 
 val create :
   port:int ->
+  configuration: Configuration.t ->
   root:string ->
   cluster:Kube_cluster.t ->
   storage:Storage.t ->
