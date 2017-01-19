d875f48752b872799e34637297f644268e64fa6c
Sebastien Mondet <seb@mondet.org>
2017-01-19 15:30:58 -0500

    Add `Local_docker` new kind of cluster
    It “does” nothing for now.

--- src/lib/cluster.ml
+++ src/lib/cluster.ml
@@ -1,8 +1,18 @@
 open Internal_pervasives
 
+type local_docker =
+  {max_jobs: int}
+[@@deriving yojson, show]
+
 type t =
   | Kube of Kube_cluster.t
+  | Local_docker of local_docker
 [@@deriving yojson, show]
+
+let kind =
+  function
+  | Kube _ -> `GCloud_kubernetes
+  | Local_docker _ -> `Local_docker
 
 let save ~storage:st cluster =
   Storage.Json.save_jsonable
@@ -16,22 +26,38 @@ let get st =
 
 let kube k = Kube k
 
+let local_docker ~max_jobs = Local_docker {max_jobs}
+
 let max_started_jobs =
   function
   | Kube k -> Kube_cluster.max_started_jobs k
+  | Local_docker { max_jobs } -> max_jobs
 
-let ensure_living =
-  function
-  | Kube k -> Kube_cluster.ensure_living k
-
-let do_log_t f ~log =
-  function
-  | Kube k -> f ~log k
-let start ~log t = do_log_t Kube_cluster.gcloud_start ~log t
-let delete ~log t = do_log_t Kube_cluster.gcloud_delete ~log t
-let describe ~log t = do_log_t Kube_cluster.gcloud_describe ~log t
+let ensure_living t ~log =
+  match t with
+  | Kube k -> Kube_cluster.ensure_living ~log k
+  | Local_docker _ ->
+    (* TODO: check docker exists and is ready *)
+    return ()
 
 let display_name =
   function
   | Kube k ->
     sprintf "GCloud-Kube-%s@%s" k.Kube_cluster.name k.Kube_cluster.zone
+  | Local_docker { max_jobs } -> sprintf "Localdocker_Max-%d" max_jobs
+
+let do_log_t f ~log =
+  function
+  | Kube k -> f ~log k
+  | Local_docker _ -> return ()
+
+let start ~log t =
+  do_log_t Kube_cluster.gcloud_start ~log t
+
+let delete ~log t = do_log_t Kube_cluster.gcloud_delete ~log t
+
+let describe ~log =
+  function
+  | Kube k -> Kube_cluster.gcloud_describe ~log k
+  | Local_docker _ as t -> return (display_name t, "")
+
--- src/lib/cluster.mli
+++ src/lib/cluster.mli
@@ -5,6 +5,8 @@ type t
 val show : t -> string
 val display_name : t -> string
 
+val kind: t -> [ `GCloud_kubernetes | `Local_docker ]
+
 val save :
   storage:Storage.t ->
   t ->
@@ -15,6 +17,9 @@ val get :
   (t, [> `Storage of [> Storage.Error.common ] ]) Deferred_result.t
 
 val kube : Kube_cluster.t -> t
+
+val local_docker: max_jobs:int -> t
+
 val max_started_jobs : t -> int
 
 val ensure_living :
